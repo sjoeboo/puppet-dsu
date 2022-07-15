03a2fa08cb6599538632e6f7b9349a2ecde2b19e
Mick Timony <michael_timony@hms.harvard.edu>
2022-07-15 16:51:12 -0400

    add debug

--- manifests/srvadmin/install.pp
+++ manifests/srvadmin/install.pp
@@ -3,17 +3,19 @@ class dsu::srvadmin::install (
   $srvadmin_version = $::dsu::srvadmin_version,
   $srvadmin_packages = $::dsu::srvadmin_packages,
   ) {
-
     if $::srvadmin_packages != undef {
+      notify{"srvadmin_packages: instal multiple packages"}
       include ::dsu::repo
       #Install select dell packages from packages Array
       $packages=hiera_array('srvadmin_packages', Data, hash, {})
       ensure_packages ($packages, {ensure => present})
     }
     elsif $::srvadmin_package != undef {
-      package{$srvadmin_package:
-        ensure  => $srvadmin_version,
-        require => Class['::dsu::repo'],
+      notify{"srvadmin_package: install single meta package"}
+      package {
+        $srvadmin_package:
+          ensure  => $srvadmin_version,
+          require => Class['::dsu::repo'],
       }
     }
 }
