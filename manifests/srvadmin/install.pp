50440775a67c881ed85481a100bbcda559babfae
Mick Timony <michael_timony@hms.harvard.edu>
2022-08-16 12:42:08 -0400

    fix notify (debug statement)

--- manifests/srvadmin/install.pp
+++ manifests/srvadmin/install.pp
@@ -3,15 +3,16 @@ class dsu::srvadmin::install (
   $srvadmin_version = $::dsu::srvadmin_version,
   $srvadmin_packages = $::dsu::srvadmin_packages,
   ) {
-    if $::srvadmin_packages != undef {
-      notify{"srvadmin_packages: instal multiple packages"}
+    notify{"start installing srvadmin packages":}
+    if $srvadmin_packages != undef {
+      notify{"srvadmin_packages: install multiple packages"}
       include ::dsu::repo
       #Install select dell packages from packages Array
       $packages=hiera_array('srvadmin_packages', Data, hash, {})
       ensure_packages ($packages, {ensure => present})
     }
-    elsif $::srvadmin_package != undef {
-      notify{"srvadmin_package: install single meta package"}
+    elsif $srvadmin_package != undef {
+      notify{"srvadmin_package: install single meta package":}
       package {
         $srvadmin_package:
           ensure  => $srvadmin_version,
