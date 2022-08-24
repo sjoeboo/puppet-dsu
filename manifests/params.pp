43b8385677f4751a90cdbeaa968b039fd0e2078b
Mick Timony <michael_timony@hms.harvard.edu>
2022-08-24 17:21:38 -0400

    dsm_om_connsvc service from is not available in version 10.x version of OpenManage as the srvadmin-cm package no longer exists

--- manifests/params.pp
+++ manifests/params.pp
@@ -9,7 +9,7 @@ class dsu::params {
   $srvadmin_package = 'srvadmin-all'
   $srvadmin_packages = ''
   $srvadmin_version = 'latest'
-  $srvadmin_services = ['dsm_sa_eventmgrd','dsm_sa_datamgrd','dsm_sa_snmpd','dsm_om_connsvc','dsm_om_shrsvc','instsvcdrv']
+  $srvadmin_services = ['dsm_sa_eventmgrd','dsm_sa_datamgrd','dsm_sa_snmpd','dsm_om_shrsvc','instsvcdrv']
   $srvadmin_services_enable = true
   $srvadmin_services_ensure = 'running'
 }
