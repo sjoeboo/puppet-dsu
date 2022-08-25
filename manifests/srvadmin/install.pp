943965871299170ae24256e2fa40ba69392fe239
Mick Timony <michael_timony@hms.harvard.edu>
2022-08-25 13:57:23 -0400

    Change conditional to check for true instead of undef .

--- manifests/srvadmin/install.pp
+++ manifests/srvadmin/install.pp
@@ -2,18 +2,18 @@ class dsu::srvadmin::install (
   $srvadmin_install_all = $::dsu::srvadmin_install_all,
   $srvadmin_install_limited = $::dsu::srvadmin_install_limited,
   $srvadmin_package = $::dsu::srvadmin_package,
-  $srvadmin_version = $::dsu::srvadmin_version,
   $srvadmin_packages = $::dsu::srvadmin_packages,
+  $srvadmin_version = $::dsu::srvadmin_version,
   ) {
     notify{"start installing srvadmin packages, limit: ${srvadmin_install_limited}, all: ${srvadmin_install_all}":}
-    if $srvadmin_install_limited != undef {
+    if $srvadmin_install_limited == true {
     notify{"srvadmin_packages: will install multiple packages":}
       include ::dsu::repo
       #Install select dell packages from packages Array
       ensure_packages ($srvadmin_packages, {ensure => present})
       notify{"srvadmin_packages: installed multiple packages: ${srvadmin_packages}":}
     }
-    elsif $srvadmin_install_all != undef {
+    elsif $srvadmin_install_all == true {
       notify{"srvadmin_package: install single meta package":}
       package {
         $srvadmin_package:
