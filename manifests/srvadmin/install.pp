72e862aa9d6b300a483d01438ff7c52d18fd9721
Mick Timony <michael_timony@hms.harvard.edu>
2022-08-25 11:28:08 -0400

    refactor, and use srvadmin_install_all to install srvadmin-all (which will install all the java stuff we do not want), and srvadmin_install_limited which will install everything but the java packages.

--- manifests/srvadmin/install.pp
+++ manifests/srvadmin/install.pp
@@ -1,17 +1,19 @@
 class dsu::srvadmin::install (
+  $srvadmin_install_all = undef,
+  $srvadmin_install_limited = undef,
   $srvadmin_package = $::dsu::srvadmin_package,
   $srvadmin_version = $::dsu::srvadmin_version,
   $srvadmin_packages = $::dsu::srvadmin_packages,
   ) {
     notify{"start installing srvadmin packages":}
-    if $srvadmin_packages != undef {
+    if $srvadmin_install_limited != undef {
     notify{"srvadmin_packages: will install multiple packages":}
       include ::dsu::repo
       #Install select dell packages from packages Array
       ensure_packages ($srvadmin_packages, {ensure => present})
       notify{"srvadmin_packages: installed multiple packages: ${srvadmin_packages}":}
     }
-    elsif $srvadmin_package != undef {
+    elsif $srvadmin_install_all != undef {
       notify{"srvadmin_package: install single meta package":}
       package {
         $srvadmin_package:
