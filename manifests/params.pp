b760980bf22c62293a2cc25d9c18a7deb4550d85
Mick Timony <michael_timony@hms.harvard.edu>
2022-08-25 11:27:14 -0400

    refactor code and add the limited package list to srvadmin_packages

--- manifests/params.pp
+++ manifests/params.pp
@@ -7,7 +7,13 @@ class dsu::params {
   $package_version = 'latest'
   $srvadmin = true
   $srvadmin_package = 'srvadmin-all'
-  $srvadmin_packages = ''
+  $srvadmin_packages = ['srvadmin-argtable2', 'srvadmin-base', 'srvadmin-deng', 'srvadmin-deng-snmp', 'srvadmin-hapi',
+    'srvadmin-idrac', 'srvadmin-idracadm7', 'srvadmin-isvc', 'srvadmin-isvc-snmp', 'srvadmin-itunnelprovider', 'srvadmin-marvellib',
+    'srvadmin-nvme', 'srvadmin-omacore', 'srvadmin-omacs', 'srvadmin-omaksmig', 'srvadmin-omcommon', 'srvadmin-omilcore', 'srvadmin-ominst',
+    'srvadmin-oslog', 'srvadmin-realssd', 'srvadmin-server-cli', 'srvadmin-server-snmp', 'srvadmin-smcommon', 'srvadmin-smweb',
+    'srvadmin-standardAgent', 'srvadmin-storage', 'srvadmin-storage-cli', 'srvadmin-storage-snmp', 'srvadmin-storageservices',
+    'srvadmin-storageservices-cli', 'srvadmin-storageservices-snmp', 'srvadmin-storelib', 'srvadmin-storelib-sysfs', 'srvadmin-sysfsutils',
+    'srvadmin-xmlsup']
   $srvadmin_version = 'latest'
   $srvadmin_services = ['dsm_sa_eventmgrd','dsm_sa_datamgrd','dsm_sa_snmpd','dsm_om_shrsvc','instsvcdrv']
   $srvadmin_services_enable = true
